171496452bd44b56e9e2effcca83c6b646d5d588
Ocean_Bear <oceanbearx@gmail.com>
2025-03-09 23:25:24 -0700

    Score finished

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -11,13 +11,14 @@ public class Coin : MonoBehaviour
         transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
     }
     // When the player enters the coin trigger, increase score and destroy the coin
+
     private void OnTriggerEnter(Collider other)
     {
         // Check if the colliding object is tagged as "Player"
         if (other.CompareTag("Player"))
         {
             // Increase the score by 1
-            ScoreManager.Instance.AddScore(1);
+            GameManager.Instance.IncreaseScore();
             // Destroy this coin object to simulate collection
             Destroy(gameObject);
         }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,10 +3,26 @@ using UnityEngine;
 
 public class GameManager : SingletonMonoBehavior<GameManager>
 {
+    [SerializeField] private int score;
+    [SerializeField] private CoinCounterUI coinCounter;
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private GameObject settingsMenu;
+
+    private bool isSettingsMenuActive;
+    public bool IsSettingsMenuActive => isSettingsMenuActive;
+
     protected override void Awake()
     {
         base.Awake();
         // Set the target frame rate to 60 frames per second
-        Application.targetFrameRate = 60;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+        coinCounter.UpdateScore(score);
     }
 }
